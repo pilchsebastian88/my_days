95ed30f5d90332cc5ecd28a09fd7e02b43d0e858
Sebastian Pilch <pilchsebastian88@gmail.com>
2022-07-12 09:10:20 +0200

    Add AppBar to MyDaysPage

--- lib/home/my_days_page.dart
+++ lib/home/my_days_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyDaysPage extends StatelessWidget {
   const MyDaysPage({
@@ -9,6 +10,13 @@ class MyDaysPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(236, 2, 172, 245),
+      appBar: AppBar(
+        title: Text(
+          'MyDays',
+          style: GoogleFonts.dancingScript(fontSize: 40, color: Colors.black),
+        ),
+        backgroundColor: const Color.fromARGB(236, 1, 189, 253),
+      ),
       body: ListView(
         children: [
           Column(
