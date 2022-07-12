b8d96bcfe422b829abc9a908d3b9cf9248869f52
Sebastian Pilch <pilchsebastian88@gmail.com>
2022-07-12 08:22:01 +0200

    Organize files

---
+++ lib/home/home_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/home_page_background.png'),
+              fit: BoxFit.cover),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'MyDays',
+                style: GoogleFonts.dancingScript(fontSize: 80),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        'learn',
+                        style: GoogleFonts.noticiaText(fontSize: 30),
+                      ),
+                      const Image(
+                        image: AssetImage('images/brain.gif'),
+                        height: 100,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'note',
+                        style: GoogleFonts.dancingScript(fontSize: 50),
+                      ),
+                      const Image(
+                        image: AssetImage('images/note.gif'),
+                        height: 60,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    '&rate',
+                    style: GoogleFonts.noticiaText(fontSize: 30),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                        size: 30,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                        size: 30,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                        size: 30,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                        size: 30,
+                      ),
+                      Icon(
+                        Icons.star_half,
+                        color: Colors.amber,
+                        size: 30,
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
