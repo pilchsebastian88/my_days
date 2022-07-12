6f48ec1b12a55834cb5da22b6dc0e8cd5e813ff4
Sebastian Pilch <pilchsebastian88@gmail.com>
2022-07-12 08:44:42 +0200

    Add MyDaysPage to project

---
+++ lib/home/my_days_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyDaysPage extends StatelessWidget {
+  const MyDaysPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(236, 2, 172, 245),
+      body: ListView(
+        children: [
+          Column(
+            children: const [],
+          )
+        ],
+      ),
+    );
+  }
+}
