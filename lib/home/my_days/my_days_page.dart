2d998469088cf5226e0d0fa78cd742ab9ecfee04
Sebastian Pilch <pilchsebastian88@gmail.com>
2022-07-13 11:05:55 +0200

    Add loop for in

--- lib/home/my_days/my_days_page.dart
+++ lib/home/my_days/my_days_page.dart
@@ -27,65 +27,66 @@ class _MyDaysPageState extends State<MyDaysPage> {
         backgroundColor: const Color.fromARGB(236, 1, 189, 253),
       ),
       body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-          stream: FirebaseFirestore.instance.collection('mydays').snapshots(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Center(
-                child: Text('Somenthing went wrong ${snapshot.hasData}'),
-              );
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final documents = snapshot.data!.docs;
-            return ListView(
-              children: [
-                Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: TextField(
-                        controller: widget.controller,
-                        decoration: InputDecoration(
-                          hintText: 'write here what you learned today',
-                          hintStyle: const TextStyle(
-                            fontSize: 11,
-                            color: Colors.yellow,
-                          ),
-                          fillColor: Colors.white38,
-                          filled: true,
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.amber.shade200,
-                              width: 1.0,
-                            ),
-                          ),
-                          focusedBorder: const OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.black, width: 1.5),
+        stream: FirebaseFirestore.instance.collection('mydays').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Somenthing went wrong ${snapshot.hasData}'),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = snapshot.data!.docs;
+          return ListView(
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextField(
+                      controller: widget.controller,
+                      decoration: InputDecoration(
+                        hintText: 'write here what you learned today',
+                        hintStyle: const TextStyle(
+                          fontSize: 11,
+                          color: Colors.yellow,
+                        ),
+                        fillColor: Colors.white38,
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.amber.shade200,
+                            width: 1.0,
                           ),
                         ),
-                        cursorColor: Colors.black,
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1.5),
+                        ),
                       ),
+                      cursorColor: Colors.black,
                     ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          primary: const Color.fromARGB(255, 48, 180, 247)),
-                      child: const Text(
-                        'save',
-                      ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        primary: const Color.fromARGB(255, 48, 180, 247)),
+                    child: const Text(
+                      'save',
                     ),
-                    NewDayWidget('My day 1'),
-                    NewDayWidget('My day 2'),
-                    NewDayWidget('My day 3'),
-                  ],
-                )
+                  ),
+                ],
+              ),
+              for (final document in documents) ...[
+                NewDayWidget(document['title']),
               ],
-            );
-          }),
+            ],
+          );
+        },
+      ),
       bottomSheet: Image.asset(
         'images/bottomsheet_image.png',
       ),
