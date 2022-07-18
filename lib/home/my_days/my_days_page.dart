5fc61dd423ba54ef7f766550fba89690d1a2f566
Sebastian Pilch <pilchsebastian88@gmail.com>
2022-07-18 19:47:50 +0200

    Organize files

--- lib/home/my_days/my_days_page.dart
+++ lib/home/my_days/my_days_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:my_days/home/my_days/add_my_days_page_content.dart';
 import 'package:my_days/home/my_days/new_day_widget.dart';
 
 class MyDaysPage extends StatefulWidget {
@@ -125,133 +126,6 @@ class _MyDaysPageState extends State<MyDaysPage> {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class AddMyDaysPageContent extends StatefulWidget {
-  const AddMyDaysPageContent({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<AddMyDaysPageContent> createState() => _AddMyDaysPageContentState();
-}
-
-class _AddMyDaysPageContentState extends State<AddMyDaysPageContent> {
-  var titleInput = '';
-  var dateInput = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('add new day'),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              onChanged: (newValue) {
-                setState(() {
-                  titleInput = newValue;
-                });
-              },
-              decoration: InputDecoration(
-                hintText: 'write here what you learned today',
-                hintStyle: const TextStyle(
-                  fontSize: 11,
-                  color: Colors.yellow,
-                ),
-                fillColor: Colors.white38,
-                filled: true,
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.amber.shade200,
-                    width: 1.0,
-                  ),
-                ),
-                focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black, width: 1.5),
-                ),
-              ),
-              cursorColor: Colors.black,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              onChanged: (newValue) {
-                setState(() {
-                  dateInput = newValue;
-                });
-              },
-              decoration: InputDecoration(
-                hintText: 'write here date yyyy.mm.dd',
-                hintStyle: const TextStyle(
-                  fontSize: 11,
-                  color: Colors.yellow,
-                ),
-                fillColor: Colors.white38,
-                filled: true,
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.amber.shade200,
-                    width: 1.0,
-                  ),
-                ),
-                focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black, width: 1.5),
-                ),
-              ),
-              cursorColor: Colors.black,
-            ),
-          ),
-          Row(
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  if (titleInput == '') {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Text('Hint'),
-                          content:
-                              const Text('You need to enter some text first'),
-                          actions: [
-                            TextButton(
-                              onPressed: () => Navigator.of(context).pop(),
-                              child: const Text('OK!'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  } else {
-                    setState(
-                      () {
-                        FirebaseFirestore.instance.collection('mydays').add(
-                          {
-                            'title': titleInput,
-                            'date': dateInput,
-                          },
-                        );
-                      },
-                    );
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: const Color.fromARGB(255, 48, 180, 247),
-                ),
-                child: const Text(
-                  'save',
-                ),
-              ),
-            ],
-          ),
-        ],
       ),
     );
   }
